f99c0fb1cec10a8b64abb22ca827dacb99ea7ef0
alan de la cruz <adelacruz-2020390@kinal.edu.gt>
2022-04-12 14:12:07 -0600

    inicio

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, render_template , redirect, url_for
+
+# Flask constructor
+app = Flask(__name__)
+
+# A decorator used to tell the application
+# which URL is associated function
+
+
+@app.route('/')
+def Contador():
+    return render_template("index.html")
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
